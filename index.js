78741fed82f930a4612938e0dbcc192cc80fa256
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-06-05 13:48:48 -0500

    info on page

--- index.js
+++ index.js
@@ -6,5 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 function displayCoffeeImage(coffee){
-     
+     let coffeeName = document.getElementById('coffee-container')
+     let name = document.createElement('p')
+     let coffeePic = document.createElement('img')
+     let coffeeHome = document.createElement('p')
+     let coffeeBean = document.createElement('p')
+
+     name.textContent = coffee.name
+     coffeePic.src = coffee.image
+     coffeeHome.textContent = coffee.country
+     coffeeBean.textContent = coffee['bean-type']
+
+     coffeeName.append(name)
+     coffeeName.append(coffeePic)
+     coffeeName.append(coffeeHome)
+     coffeeName.append(coffeeBean)
+
 }
