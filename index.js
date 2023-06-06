f49b21300df23ada500940b099fafd509699c59b
harjas816 <harjas816@gmail.com>
2023-06-06 10:09:31 -0400

    starting to add carousel

--- index.js
+++ index.js
@@ -5,21 +5,35 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-function displayCoffeeImage(coffee){
-     let coffeeName = document.getElementById('coffee-container')
-     let name = document.createElement('p')
-     let coffeePic = document.createElement('img')
-     let coffeeHome = document.createElement('p')
-     let coffeeBean = document.createElement('p')
+// function displayCoffeeImage(coffee){
+//      let coffeeName = document.getElementById('coffee-container')
+//      let name = document.createElement('p')
+//      let coffeePic = document.createElement('img')
+//      let coffeeHome = document.createElement('p')
+//      let coffeeBean = document.createElement('p')
 
-     name.textContent = coffee.name
-     coffeePic.src = coffee.image
-     coffeeHome.textContent = coffee.country
-     coffeeBean.textContent = coffee['bean-type']
+//      name.textContent = coffee.name
+//      coffeePic.src = coffee.image
+//      coffeeHome.textContent = coffee.country
+//      coffeeBean.textContent = coffee['bean-type']
 
-     coffeeName.append(name)
-     coffeeName.append(coffeePic)
-     coffeeName.append(coffeeHome)
-     coffeeName.append(coffeeBean)
+//      coffeeName.append(name)
+//      coffeeName.append(coffeePic)
+//      coffeeName.append(coffeeHome)
+//      coffeeName.append(coffeeBean)
 
+// }
+
+function addToCarousel(coffee){
+    let carouselTrackContainer = document.getElementById("carousel-track-container")
+    let carouselTrack = document.getElementsByClassName("carousel-track")
+    let coffeeSlide = document.createElement("li")
+    
+    coffeeSlide.className = "carousel-slide"
+    let coffeePic = document.createElement("img")
+    coffeePic.src = coffee.image
+
+    coffeeSlide.appendChild(coffeePic)
+    carouselTrack.appendChild(coffeeSlide)
+    carouselTrackContainer.appendChild(carouselTrack)
 }
